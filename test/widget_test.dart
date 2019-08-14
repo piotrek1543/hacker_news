d5a0675dde2b2684b528cf4ddeac795462d1c8b1
Piotr Ekert <piotrekert90@gmail.com>
2019-08-14 21:30:20 +0200

    Add unit tests for Headline widget. Update widget_test.dart.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,6 @@
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hacker_news/main.dart';
@@ -20,4 +19,5 @@ void main() {
 
     expect(find.byIcon(Icons.launch), findsOneWidget);
   }, skip: true);
+
 }
