200164ef175d35c0f3f37fc30754397f0bfd0249
Piotr Ekert <piotrekert90@gmail.com>
2019-08-15 14:08:28 +0200

    Add assertions for text and textColor change. Update headline.dart.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,21 +3,108 @@
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
+// import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hacker_news/main.dart';
+import 'package:hacker_news/src/widgets/headline.dart';
 
 void main() {
-  testWidgets('clicking tile opens it', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('headline animates and changes text correctly',
+      (WidgetTester tester) async {
+    String text = "Foo";
+    int index = 0;
+    Key buttonKey = GlobalKey();
+    Key headlineKey = GlobalKey();
 
-    expect(find.byIcon(Icons.launch), findsNothing);
+    Widget widget = StatefulBuilder(
+      builder: (BuildContext context, void Function(void Function()) setState) {
+        return Directionality(
+          textDirection: TextDirection.ltr,
+          child: Column(
+            children: <Widget>[
+              Headline(
+                key: headlineKey,
+                text: text,
+                index: index,
+              ),
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    text = 'Bar';
+                    index = 1;
+                  });
+                },
+                child: Text("Tap"),
+                key: buttonKey,
+              )
+            ],
+          ),
+        );
+      },
+    );
+    await tester.pumpWidget(
+      widget,
+    );
 
-    await tester.tap(find.byType(ExpansionTile).first);
+    expect(find.text('Foo'), findsOneWidget);
+
     await tester.pump();
 
-    expect(find.byIcon(Icons.launch), findsOneWidget);
-  }, skip: true);
+    await tester.tap(find.byKey(buttonKey));
 
+    await tester.pumpAndSettle();
+
+    expect(find.text('Bar'), findsOneWidget);
+  });
+
+  testWidgets('headline animates and changes text color correctly',
+      (WidgetTester tester) async {
+    String text = "Foo";
+    int index = 0;
+    Key buttonKey = GlobalKey();
+    Key headlineKey = GlobalKey();
+    Headline headline;
+
+    Widget widget = StatefulBuilder(
+      builder: (BuildContext context, void Function(void Function()) setState) {
+        headline = Headline(
+          key: headlineKey,
+          text: text,
+          index: index,
+        );
+
+        return Directionality(
+          textDirection: TextDirection.ltr,
+          child: Column(
+            children: <Widget>[
+              headline,
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    text = 'Bar';
+                    index = 1;
+                  });
+                },
+                child: Text("Tap"),
+                key: buttonKey,
+              )
+            ],
+          ),
+        );
+      },
+    );
+    await tester.pumpWidget(
+      widget,
+    );
+
+    expect(headline.targetColor, headlineTextColors[index]);
+
+    await tester.pump();
+
+    await tester.tap(find.byKey(buttonKey));
+
+    await tester.pumpAndSettle();
+
+    expect(headline.targetColor, headlineTextColors[index]);
+  });
 }
