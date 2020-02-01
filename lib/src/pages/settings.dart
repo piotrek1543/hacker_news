472c35d56e66be56b37c76e8f63caf05a6a034da
Piotr <piotrekert90@gmail.com>
2020-02-01 10:30:14 +0100

    Add dark mode  Switch widget to SettingsPage.

--- lib/src/pages/settings.dart
+++ lib/src/pages/settings.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hacker_news/src/notifiers/prefs.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Text('Settings'),
+      body: Column(
+        children: <Widget>[
+          Text('Use Dark Mode'),
+          Switch(
+            value: Provider
+                .of<PrefsNotifier>(context)
+                .userDarkMode,
+            onChanged: (bool newValue) {
+              Provider
+                  .of<PrefsNotifier>(context)
+                  .userDarkMode = newValue;
+            },
+          ),
+        ],
       ),
     );
   }
